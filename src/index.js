7241a8c99e5cfdfb744d6d9f60de20110dfbd5b2
ninjaaung <ninja.aung02@gmail.com>
2020-05-09 04:24:53 -0700

    Mongoose needs node back-end

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import mongoose from "mongoose";
 
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/react-quiz`);
+
+// Nodd.js Server Back-end
+//mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/react-quiz`);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
